c6c94914d004434731e8890c4fce6f2ac3bba2e2
Ousmane Barry <79618101+ousmanebarry@users.noreply.github.com>
2021-02-28 17:37:34 -0500

    code structure improvement

--- python build/main.py
+++ python build/main.py
@@ -6,7 +6,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 
-
 driver = webdriver.Chrome(executable_path="/Users/Owner/Documents/chromedriver.exe")
 driver.get("https://play.typeracer.com/")
 page_loaded = False
@@ -24,20 +23,26 @@ except TimeoutException:
 
 def type_bot():
     for word in my_text:
-        pyautogui.typewrite(word)
+        pyautogui.typewrite(word, interval=0.2)
+    WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'gwt-uid-17')))
+    driver.find_element_by_xpath('//*[@id="gwt-uid-17"]/table/tbody/tr[3]/td/table/tbody/tr/td[2]/a').click()
 
 
 if page_loaded:
     driver.find_element_by_xpath('//*[@id="gwt-uid-1"]/a').click()
-    WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="gwt-uid-17"]/table/tbody'
-                                                                                 '/tr[2]/td/table/tbody/tr[ '
-                                                                                 '1]/td/table/tbody/tr[1]/td/div/div')))
-    my_text = driver.find_element_by_xpath('//*[@id="gwt-uid-17"]/table/tbody/tr[2]/td/table/tbody/tr['
-                                           '1]/td/table/tbody/tr[1]/td/div/div').text
-    input_box = driver.find_element_by_xpath('//*[@id="gwt-uid-17"]/table/tbody/tr[2]/td/table/tbody/tr[2]/td/input')
+    while True:
+        WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="gwt-uid-17"]/table/tbody'
+                                                                                     '/tr[2]/td/table/tbody/tr[ '
+                                                                                     '1]/td/table/tbody/tr['
+                                                                                     '1]/td/div/div')))
+        my_text = driver.find_element_by_xpath('//*[@id="gwt-uid-17"]/table/tbody/tr[2]/td/table/tbody/tr['
+                                               '1]/td/table/tbody/tr[1]/td/div/div').text
+        input_box = driver.find_element_by_xpath(
+            '//*[@id="gwt-uid-17"]/table/tbody/tr[2]/td/table/tbody/tr[2]/td/input')
 
-    while not timer_finished:
-        input_box_class = input_box.get_attribute('class')
-        if input_box_class == 'txtInput':
-            timer_finished = True
-            type_bot()
+        while not timer_finished:
+            input_box_class = input_box.get_attribute('class')
+            if input_box_class == 'txtInput':
+                timer_finished = True
+                type_bot()
+                timer_finished = False
